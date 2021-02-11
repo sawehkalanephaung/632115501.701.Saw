62e73f427d070df8f611e6894d6cd74c4a83d518
sawehkalanephaung <77400484+sawehkalanephaung@users.noreply.github.com>
2021-02-11 21:32:37 +0700

    Add files via upload
    Week 10 lab submission.

---
+++ Human.java
@@ -0,0 +1,23 @@
+package lab10;
+
+public class Human{
+   public void hit ( Animal animal){
+       System.out.println(animal.roar());
+   }
+   public static void main(String[] args) {
+       Animal dog = new Dog(); // questio 1
+       Human human = new Human();
+       Human person = new Human(); // update the main method of Person
+       System.out.println(" The first calling hit ( Animal)");
+       human.hit(dog); // Quetion 2
+
+      dog = new Cat();
+       System.out.println( " The second calling hit ( Animal )");
+       human.hit(dog);// Question 3
+
+       dog = new Fish();
+       System.out.println(" The Third calling hit ( Animal ");
+       person.hit(dog);
+   }
+}
+
